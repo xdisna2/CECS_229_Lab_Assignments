31dcf5356d83a82f0308892a2004a78dd049297b
Anthony Le <aznthonyle45@gmail.com>
2021-04-19 13:53:57 -0700

    Lab Finished

--- Lab 6/main.py
+++ Lab 6/main.py
@@ -14,21 +14,21 @@ col_num = 0
 birth_data = pd.read_csv("birthweight.csv")
 
 # We only want the x-axis data to be compared to be Birthweight
-x_axis = birth_data['Birthweight'].to_numpy()
+y_axis = birth_data['Birthweight'].to_numpy()
 
 # Loop through the columns, exception of Birthweight skip
 for col in birth_data:
     # Basically skip the first value and set the y_axis to be the data column
     if col != 'Birthweight':
         # Set the column of the data to the y_axis via Numpy
-        y_axis = birth_data[col].to_numpy()
+        x_axis = birth_data[col].to_numpy()
 
         # Plot the scatter graph
         plt.scatter(x_axis, y_axis, color='red')
 
         # Set the axis Labels
-        plt.xlabel('Birthweight')
-        plt.ylabel(col_list[col_num])
+        plt.xlabel(col_list[col_num])
+        plt.ylabel('Birthweight')
 
         # Set the title
         plt.title(f"Birthweight vs. {col_list[col_num]}")
